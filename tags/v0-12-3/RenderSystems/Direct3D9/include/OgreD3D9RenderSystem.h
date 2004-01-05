6c1cd3b652e39e69706ac3414a2e1f0728cae6a0
nobody <nobody@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-01-05 12:23:09 +0000

    This commit was manufactured by cvs2svn to create tag 'v0-12-3'.

---
+++ tags/v0-12-3/RenderSystems/Direct3D9/include/OgreD3D9RenderSystem.h
@@ -0,0 +1,205 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2002 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+#ifndef __D3D9RENDERSYSTEM_H__
+#define __D3D9RENDERSYSTEM_H__
+
+#include "OgreD3D9Prerequisites.h"
+#include "OgreString.h"
+#include "OgreStringConverter.h"
+#include "OgreRenderSystem.h"
+#include "OgreD3D9Mappings.h"
+
+#include "OgreNoMemoryMacros.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <dxerr9.h>
+#include "OgreMemoryMacros.h"
+
+namespace Ogre 
+{
+#define MAX_LIGHTS 8
+
+	class D3D9DriverList;
+	class D3D9Driver;
+
+	/**
+	Implementation of DirectX9 as a rendering system.
+	*/
+	class D3D9RenderSystem : public RenderSystem
+	{
+	private:
+		/// Direct3D
+		LPDIRECT3D9			mpD3D;
+		/// Direct3D rendering device
+		LPDIRECT3DDEVICE9	mpD3DDevice;
+		
+		// Stored options
+		ConfigOptionMap mOptions;
+        /// wait for vsync
+		bool mVSync;
+		/// full-screen multisampling antialiasing type
+		D3DMULTISAMPLE_TYPE mFSAAType;
+		/// full-screen multisampling antialiasing level
+		DWORD mFSAAQuality;
+
+		/// external window handle ;)
+		HWND mExternalHandle;
+		/// instance
+		HINSTANCE mhInstance;
+
+		/// List of D3D drivers installed (video cards)
+		D3D9DriverList* mDriverList;
+		/// Currently active driver
+		D3D9Driver* mActiveD3DDriver;
+		/// Device caps.
+		D3DCAPS9 mCaps;
+
+		/// structure holding texture unit settings for every stage
+		struct sD3DTextureStageDesc
+		{
+			/// the type of the texture
+			D3D9Mappings::eD3DTexType texType;
+			/// wich texCoordIndex to use
+			int coordIndex;
+			/// type of auto tex. calc. used
+			TexCoordCalcMethod autoTexCoordType;
+			/// texture 
+			IDirect3DBaseTexture9 *pTex;
+		} mTexStageDesc[OGRE_MAX_TEXTURE_LAYERS];
+
+		// Array of up to 8 lights, indexed as per API
+		// Note that a null value indeicates a free slot
+		Light* mLights[MAX_LIGHTS];
+
+		D3D9DriverList* getDirect3DDrivers(void);
+		void refreshD3DSettings(void);
+
+		inline bool compareDecls( D3DVERTEXELEMENT9* pDecl1, D3DVERTEXELEMENT9* pDecl2, int size );
+
+		// Matrix conversion
+		D3DXMATRIX makeD3DXMatrix( const Matrix4& mat );
+		Matrix4 convertD3DXMatrix( const D3DXMATRIX& mat );
+
+		void initInputDevices(void);
+		void processInputDevices(void);
+		void setD3D9Light( int index, Light* light );
+		
+		// state management methods, very primitive !!!
+		HRESULT __SetRenderState(D3DRENDERSTATETYPE state, DWORD value);
+		HRESULT __SetSamplerState(DWORD sampler, D3DSAMPLERSTATETYPE type, DWORD value);
+		HRESULT __SetTextureStageState(DWORD stage, D3DTEXTURESTAGESTATETYPE type, DWORD value);
+
+		/// return anisotropy level
+		DWORD _getCurrentAnisotropy(int unit);
+		/// check if a FSAA is supported
+		bool _checkMultiSampleQuality(D3DMULTISAMPLE_TYPE type, DWORD *outQuality, D3DFORMAT format, UINT adapterNum, D3DDEVTYPE deviceType, BOOL fullScreen);
+		/// set FSAA
+		void _setFSAA(D3DMULTISAMPLE_TYPE type, DWORD qualityLevel);
+		
+		D3D9HardwareBufferManager* mHardwareBufferManager;
+		size_t mLastVertexSourceCount;
+
+        /// Flag to indicate normal normalisation is forced
+        bool mForcedNormalisation;
+
+	public:
+		// constructor
+		D3D9RenderSystem( HINSTANCE hInstance );
+		// destructor
+		~D3D9RenderSystem();
+
+		virtual initConfigOptions(void);
+		// method for resizing/repositing the render window
+ 		virtual ResizeRepositionWindow(HWND wich);
+		// method for setting external window hwnd
+		void SetExternalWindowHandle(HWND externalHandle){mExternalHandle = externalHandle;};
+
+		// Overridden RenderSystem functions
+		ConfigOptionMap& getConfigOptions(void);
+		String validateConfigOptions(void);
+		RenderWindow* initialise( bool autoCreateWindow );
+		RenderWindow* createRenderWindow(const String &name, int width, int height, int colourDepth, bool fullScreen, int left = 0, int top = 0, bool depthBuffer = true, RenderWindow* parentWindowHandle = 0);
+		RenderTexture * createRenderTexture( const String & name, int width, int height );
+		String getErrorDescription( long errorNumber );
+		const String& getName(void) const;
+		// Low-level overridden members
+		void setConfigOption( const String &name, const String &value );
+		void reinitialise();
+		void shutdown();
+		void startRendering();
+		void setAmbientLight( float r, float g, float b );
+		void setShadingType( ShadeOptions so );
+		void setLightingEnabled( bool enabled );
+		void destroyRenderWindow( RenderWindow* pWin );
+		void convertColourValue( const ColourValue& colour, unsigned long* pDest );
+		void setStencilCheckEnabled(bool enabled);
+		void setStencilBufferFunction(CompareFunction func);
+		void setStencilBufferReferenceValue(ulong refValue);
+		void setStencilBufferMask(ulong mask);
+		void setStencilBufferFailOperation(StencilOperation op);
+		void setStencilBufferDepthFailOperation(StencilOperation op);
+		void setStencilBufferPassOperation(StencilOperation op);
+        void setNormaliseNormals(bool normalise);
+
+		// Low-level overridden members, mainly for internal use
+		void _addLight( Light* lt );
+		void _removeLight( Light* lt );
+		void _modifyLight( Light* lt );
+		void _removeAllLights(void);
+		void _pushRenderState(void);
+		void _popRenderState(void);
+		void _setWorldMatrix( const Matrix4 &m );
+		void _setViewMatrix( const Matrix4 &m );
+		void _setProjectionMatrix( const Matrix4 &m );
+		void _setSurfaceParams( const ColourValue &ambient, const ColourValue &diffuse, const ColourValue &specular, const ColourValue &emissive, Real shininess );
+		void _setTexture( int unit, bool enabled, const String &texname );
+        void _setTextureCoordSet( int stage, int index );
+        void _setTextureCoordCalculation(int unit, TexCoordCalcMethod m);
+		void _setTextureBlendMode( int stage, const LayerBlendModeEx& bm );
+		void _setTextureAddressingMode( int stage, Material::TextureLayer::TextureAddressingMode tam );
+		void _setTextureMatrix( int stage, const Matrix4 &xform );
+		void _setSceneBlending( SceneBlendFactor sourceFactor, SceneBlendFactor destFactor );
+		void _setAlphaRejectSettings( CompareFunction func, unsigned char value );
+		void _setViewport( Viewport *vp );
+		void _beginFrame(void);
+		void _endFrame(void);
+		void _setCullingMode( CullingMode mode );
+		void _setDepthBufferParams( bool depthTest = true, bool depthWrite = true, CompareFunction depthFunction = CMPF_LESS_EQUAL );
+		void _setDepthBufferCheckEnabled( bool enabled = true );
+		void _setDepthBufferWriteEnabled(bool enabled = true);
+		void _setDepthBufferFunction( CompareFunction func = CMPF_LESS_EQUAL );
+		void _setDepthBias(ushort bias);
+		void _setFog( FogMode mode = FOG_NONE, ColourValue colour = ColourValue::White, Real expDensity = 1.0, Real linearStart = 0.0, Real linearEnd = 1.0 );
+		void _makeProjectionMatrix(Real fovy, Real aspect, Real nearPlane, Real farPlane, Matrix4& dest);
+		void _setRasterisationMode(SceneDetailLevel level);
+		void _setTextureLayerFiltering(int unit, const TextureFilterOptions texLayerFilterOps);
+		void _setTextureLayerAnisotropy(int unit, int maxAnisotropy);
+		void setVertexDeclaration(VertexDeclaration* decl);
+		void setVertexBufferBinding(VertexBufferBinding* binding);
+        void _render(const RenderOperation& op);
+
+	};
+}
+#endif
---
+++ tags/v0-12-3/RenderSystems/GL/include/OgreGLSupport.h
@@ -0,0 +1,119 @@
+#ifndef OGRE_GLSUPPORT_H
+#define OGRE_GLSUPPORT_H
+
+#include "OgreGLPrerequisites.h"
+#include "OgreGLRenderSystem.h"
+
+#include "OgreRenderWindow.h"
+#include "OgreConfigOptionMap.h"
+
+namespace Ogre
+{
+    
+class GLSupport
+{
+public:
+    GLSupport() { }
+    virtual ~GLSupport() 
+    { 
+        // XXX Is this really necessary?
+        extensionList.clear();
+    }
+
+    /**
+    * Add any special config values to the system.
+    * Must have a "Full Screen" value that is a bool and a "Video Mode" value
+    * that is a string in the form of wxh
+    */
+    virtual void addConfig() = 0;
+
+	virtual void setConfigOption(const String &name, const String &value);
+
+    /**
+    * Make sure all the extra options are valid
+    * @return string with error message
+    */
+    virtual String validateConfig() = 0;
+
+	virtual ConfigOptionMap& getConfigOptions(void);
+
+    
+	virtual RenderWindow* createWindow(bool autoCreateWindow, GLRenderSystem* renderSystem) = 0;
+
+	/**
+    * Create a specific instance of a render window
+    */
+    virtual RenderWindow* newWindow(const String& name, int width, int height, int colourDepth,
+            bool fullScreen, int left, int top, bool depthBuffer, RenderWindow* parentWindowHandle,
+			bool vsync) = 0;
+
+    /**
+    * Start anything special
+    */
+    virtual void start() = 0;
+    /**
+    * Stop anything special
+    */
+    virtual void stop() = 0;
+
+    /**
+    * Set the correct context as active
+    */
+    virtual void begin_context()
+    { }
+
+    /**
+    * Stop the currrent context
+    */
+    virtual void end_context()
+    { }
+
+    /**
+    * get vendor information
+    */
+    const String& getGLVendor(void)
+    {
+        return mVendor;
+    }
+
+    /**
+    * get version information
+    */
+    const String& getGLVersion(void)
+    {
+        return mVersion;
+    }
+
+    /**
+    * compare GL version numbers
+    */
+    bool checkMinGLVersion(const String& v);
+
+    /**
+    * Check if an extension is available
+    */
+    virtual bool checkExtension(const String& ext);
+    /**
+    * Get the address of a function
+    */
+    virtual void* getProcAddress(const String& procname) = 0;
+    /** Intialises GL extensions, must be done AFTER the GL context has been
+        established.
+    */
+    virtual void initialiseExtensions(void);
+
+protected:
+	// Stored options
+    ConfigOptionMap mOptions;
+
+private:
+    // This contains the complete list of supported extensions
+    std::set<String> extensionList;
+    String mVersion;
+    String mVendor;
+
+}; // class GLSupport
+
+}; // namespace Ogre
+
+#endif // OGRE_GLSUPPORT_H
